ef14b953acacd4f3148e9872d4016285f6fdade7
mack1ch <dimastepanov.1995@mail.ru>
2024-06-05 00:32:46 +0500

    added: duplicated training

--- src/features/profile-slice/profileCard/ui/ui.tsx
+++ src/features/profile-slice/profileCard/ui/ui.tsx
@@ -41,11 +41,11 @@ export const ProfileCard = () => {
                 </p>
               </div>
             </div>
-            {user.trainerProfile?.studio?.name && (
-              <div className={styles.studio}>
-                {user.trainerProfile?.studio?.name}
+            {user.trainerProfile?.studios?.map((studio) => (
+              <div key={studio.id} className={styles.studio}>
+                {studio.name}
               </div>
-            )}
+            ))}
             <div className={styles.tags}>
               {user.trainerProfile?.experience && (
                 <div className={styles.experience}>
